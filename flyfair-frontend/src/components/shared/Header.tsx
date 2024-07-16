35bf81039c0eaf1648378ae0d72090a672dfa81e
m0nztrum <delafiado@gmail.com>
2024-07-16 20:31:22 +0000

    feat: dir restructure

---
+++ flyfair-frontend/src/components/shared/Header.tsx
@@ -0,0 +1,9 @@
+export const Header = () => {
+    return (
+        <div className="p-4">
+            <span className="text-left text-3xl font-extrabold text-white">
+                flyfair
+            </span>
+        </div>
+    );
+};
