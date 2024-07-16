693497ec986e72bb09c7d560f2624a747b78e349
m0nztrum <delafiado@gmail.com>
2024-07-16 20:31:27 +0000

    feat: dir restructure

--- src/components/shared/Header.tsx
+++
@@ -1,9 +0,0 @@
-export const Header = () => {
-    return (
-        <div className="p-4">
-            <span className="text-left text-3xl font-extrabold text-white">
-                flyfair
-            </span>
-        </div>
-    );
-};
