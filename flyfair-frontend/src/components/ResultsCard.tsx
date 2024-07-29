6a1be3779e89ab2087e638e66cbeba7959d186a9
m0nztrum <delafiado@gmail.com>
2024-07-29 23:27:32 +0000

    feat: api call

--- flyfair-frontend/src/components/ResultsCard.tsx
+++ flyfair-frontend/src/components/ResultsCard.tsx
@@ -1,9 +1,35 @@
-export const ResultsCard = ({ flights: [] }) => {
+import { formatDuration } from '../utils/formatDuration';
+import { formatTime } from '../utils/formatTime';
+
+type Flight = {
+    departTime: string;
+    arriveTime: string;
+    duration: string;
+    airline: string;
+};
+
+type FlightResultProps = {
+    flights: Flight[];
+};
+
+export const ResultsCard = ({ flights }: FlightResultProps) => {
     return (
-        <div>
-            <p>flight depart time</p>
-            <div>
-                <p>Currency</p>
+        <div className="mt-4 flex h-full justify-center">
+            <div className="rounded-lg border p-5 shadow-lg">
+                {/* first line */}
+                <div>
+                    <p className="text-lg font-medium">
+                        {formatTime(flights.departTime)} -{' '}
+                        {formatTime(flights.arriveTime)}
+                    </p>
+                    <p className="mt-2 text-sm sm:mt-0">
+                        {formatDuration(flights.duration)}
+                    </p>
+                </div>
+                {/* Second line */}
+                <div>
+                    <p>{flights.airline}</p>
+                </div>
             </div>
         </div>
     );
--- flyfair-frontend/src/components/SearchForm.tsx
+++ flyfair-frontend/src/components/SearchForm.tsx
@@ -1,16 +1,64 @@
 import { useState } from 'react';
 import { InputField } from './InputField';
+import { ResultsCard } from './ResultsCard';
 
 export const SearchForm = () => {
     const [departFrom, setDepartFrom] = useState<string>('');
     const [arriveAt, setArriveAt] = useState<string>('');
     const [departDate, setDepartDate] = useState<string>('');
     const [arriveDate, setArriveDate] = useState<string>('');
+    const [searching, setSearching] = useState<boolean>(false);
+    const [hasSearched, setHasSearched] = useState<boolean>(false);
+    const [flightResults, setFlightResults] = useState([]);
+    const [error, setError] = useState<string>('');
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        setHasSearched(true);
+
+        if (departFrom === arriveAt) {
+            setError('Departure and destination cannot be the same');
+            return;
+        }
+        setSearching(true);
+        setError('');
+
+        try {
+            const response = await fetch(
+                'https://flyfare.onrender.com/search',
+                {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({
+                        departFrom,
+                        arriveAt,
+                        departDate,
+                        arriveDate,
+                    }),
+                },
+            );
+
+            if (response.ok) {
+                const data = await response.json();
+                setFlightResults(data);
+            } else {
+                setFlightResults([]);
+                console.error('Error fetching flight prices');
+                setError('');
+            }
+        } catch (error) {
+            console.error('Error in handleSubmit: ', error.message);
+        } finally {
+            setSearching(false);
+        }
+    };
 
     return (
         <div className="mx-auto w-screen px-4">
             <div className="rounded-lg bg-white p-6 opacity-85">
-                <form>
+                <form onSubmit={handleSubmit}>
                     <div className="flex justify-center space-x-4">
                         <InputField
                             inputtype="text"
@@ -42,10 +90,29 @@ export const SearchForm = () => {
                             type="submit"
                             className="rounded-lg bg-gray-500 px-6 py-2 font-semibold"
                         >
-                            Search
+                            {searching ? 'Searching...' : 'Search'}
                         </button>
                     </div>
                 </form>
+                {error && (
+                    <div className="mt-4 text-center text-red-600">{error}</div>
+                )}
+            </div>
+            {/* Results display section */}
+            <div>
+                {searching ? (
+                    <div className="p-2 text-center font-bold text-white">
+                        Loading...
+                    </div>
+                ) : hasSearched && flightResults.length === 0 ? (
+                    <div className="text-center font-extrabold text-red-500">
+                        No flights found
+                    </div>
+                ) : (
+                    flightResults.map((flight, index) => (
+                        <ResultsCard key={index} flights={flight} />
+                    ))
+                )}
             </div>
         </div>
     );
